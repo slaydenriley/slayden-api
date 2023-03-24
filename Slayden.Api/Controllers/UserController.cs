1714db80fda5ccd284eab74550af1d900e24e72b
Riley Slayden <slaydenriley@gmail.com>
2023-03-23 21:39:28 -0500

    Basic controllers

--- Slayden.Api/Controllers/UserController.cs
+++ Slayden.Api/Controllers/UserController.cs
@@ -1,6 +1,38 @@
-namespace Slayden.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Slayden.Api.Data;
+using Slayden.Api.Models;
 
-public class UserController
+namespace Slayden.Api.Controllers
 {
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly SlaydenDbContext _context;
+
+        public UsersController(SlaydenDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _context.User.ToList();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.User.Add(user);
+                _context.SaveChanges();
+                return CreatedAtAction(nameof(GetAll), new { id = user.Id }, user);
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
 }
